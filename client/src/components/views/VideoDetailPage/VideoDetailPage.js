74ee242f1221b2fd6f81e91ca0419ae794e7bcf2
unknown <swc4327@naver.com>
2022-02-23 14:21:07 +0900

    자신의 동영상일 경우 구독버튼 없게 수정

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -21,6 +21,9 @@ function VideoDetailPage(props) {
   }, []);
 
   if (VideoDetail.writer) {
+
+    const subscribeButton = VideoDetail.writer._id !== localStorage.getItem('userId') && <Subscribe userTo={VideoDetail.writer._id} userFrom={localStorage.getItem('userId')}/>
+
     return (
       <Row gutter={[16, 16]}>
         <Col lg={18} xs={24}>
@@ -31,7 +34,7 @@ function VideoDetailPage(props) {
               controls
             />
 
-            <List.Item actions={[<Subscribe userTo={VideoDetail.writer._id} userFrom={localStorage.getItem('userId')}/>]}>
+            <List.Item actions={[subscribeButton]}>
               <List.Item.Meta
                 avatar={<Avatar src={VideoDetail.writer.image} />}
                 title={VideoDetail.writer.name}
